60ff0843632b19be3768e0b5151559885b3befa7
swapnil ingale <30555446+swapnil0@users.noreply.github.com>
2022-04-02 17:23:33 +0530

    project structure updated

---
+++ src/main/java/com/mmit/health/HealthData/HealthDataApplication.java
@@ -0,0 +1,13 @@
+package com.mmit.health.HealthData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthDataApplication.class, args);
+	}
+
+}
